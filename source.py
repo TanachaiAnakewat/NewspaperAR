7e2a185c9b949ef4db3148e9197f4e66eab2bc36
TanachaiAnakewat <thunanakewat@gmail.com>
2022-12-05 21:06:47 +0900

    initial push

---
+++ source.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img_rgb = cv2.imread('mario.png')
+template = cv2.imread('mario_coin.png')
+w, h = template.shape[:-1]
+
+res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+threshold = .8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):  # Switch collumns and rows
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+cv2.imwrite('result.png', img_rgb)
